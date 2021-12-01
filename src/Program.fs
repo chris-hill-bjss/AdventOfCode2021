4ad09c1219914be7cbbb0cb27f01ad06d67aeeb7
Chris Hill <chris@Chriss-MacBook-Pro.local>
2021-12-01 21:44:15 +0000

    Initial check in and day one test input solve.

---
+++ src/Program.fs
@@ -0,0 +1,31 @@
+﻿open Hopac
+open HttpFs.Client
+
+let body =
+  Request.createUrl Get "https://adventofcode.com/2021/day/1/input"
+  |> Request.responseAsString
+  |> run
+
+let inputs = [|
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
+|]
+
+[<EntryPoint>]
+let Main(_) =
+
+    let increments = 
+        [| for x, y in Array.zip inputs[..inputs.Length - 2] inputs[1..] -> if y > x then 1 else 0 |] 
+        |> Array.sum
+
+    printfn "%A" increments
+
+    0
